948743c5e35b377388c487c41c535deb4775a1f6
Keyur Golani <keyurrgolani@gmail.com>
2016-05-05 00:39:01 -0700

    Final Changes for presentation

--- app.js
+++ app.js
@@ -53,12 +53,12 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-// 
+//
 
 
 
 
-app.get('/Oceanography', function(req, res) {
+app.get('/Oceanology', function(req, res) {
 	   res.render('presentation/oceanologist', { });
 	});
 
